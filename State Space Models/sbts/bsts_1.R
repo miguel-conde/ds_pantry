60150a32bb59ea92702dcf1f51086da8f7049f8f
ES\mcondedesimon <mcondedesimon@deloittedigital.es>
2020-09-15 08:26:54 +0200

    Bsts.R

--- State Space Models/sbts/bsts_1.R
+++ State Space Models/sbts/bsts_1.R
@@ -153,3 +153,101 @@ reg_contr_array %>% apply(1:2, mean) %>% rowSums() %>% plot(type = "l")
 all_contr <- abind::abind(model_alshaya$state.contributions, along = 2)
 
 (all_contr %>% apply(2:3, mean) %>% t)[, "regression"]  %>% plot(type = "l")
+
+
+
+# Reproducción de los resultados de Alshaya -------------------------------
+TARGET_VAR <- "sessions"
+VARS_MODEL <- c("criteo_impressions_h_as" ,          "facebook_impressions_h_as" ,       
+                "gdn_impressions_h_as"      ,        "goog_trends"     ,                 
+                "non_brand_search_impressions_h_as", "online_promo_discount_on" ,        
+                "online_promo_sale_on"        ,      "ramadan_on"        ,               
+                "snapchat_impressions_h_as" )
+f <- as.formula(paste(TARGET_VAR, "~",
+                      paste(VARS_MODEL, collapse="+"),
+                      "- 1"))
+
+ss <- list()
+ss <- AddSeasonal(ss, op_base_online %>% pull(TARGET_VAR), nseasons = 7)
+model_alshaya <- bsts(f,
+                      state.specification = ss,
+                      data = op_base_online,
+                      niter = 1000,
+                      expected.model.size = 9)
+
+plot(model_alshaya)
+lines(model_alshaya$original.series)
+plot(model_alshaya, "components")  
+
+model_alshaya$coefficients %>% colMeans()
+
+mape <- mean(abs(model_alshaya$one.step.prediction.errors / 
+                   sum(model_alshaya$original.series))^2)
+mape
+
+# Dynamic Regression ------------------------------------------------------
+
+TARGET_VAR <- "sessions"
+VARS_MODEL <- c("criteo_impressions_h_as" ,          "facebook_impressions_h_as" ,       
+                "gdn_impressions_h_as"      ,        # "goog_trends"     ,                 
+                "non_brand_search_impressions_h_as", "online_promo_discount_on" ,        
+                "online_promo_sale_on"        ,      "ramadan_on"        ,               
+                "snapchat_impressions_h_as" )
+f <- as.formula(paste(TARGET_VAR, "~",
+                      paste(VARS_MODEL, collapse="+"),
+                      "- 1"))
+
+ss <- AddLocalLinearTrend(list(), op_base_online %>% pull(TARGET_VAR))
+ss <- AddSeasonal(ss, op_base_online %>% pull(TARGET_VAR), nseasons = 7)
+ss <- AddDynamicRegression(ss, f,data = op_base_online)
+model_alshaya <- bsts(op_base_online %>% pull(TARGET_VAR),
+                      state.specification = ss,
+                      # data = op_base_online,
+                      niter = 1000,
+                      expected.model.size = 9)
+
+names(model_alshaya)
+
+plot(model_alshaya)
+lines(model_alshaya$original.series)
+plot(model_alshaya, "components")  
+
+# pred1 <- predict(model_alshaya, horizon = 30)
+# plot(pred1, plot.original = 120)
+
+mape <- mean(abs(model_alshaya$one.step.prediction.errors / 
+                   sum(model_alshaya$original.series))^2)
+mape
+
+model_alshaya$dynamic.regression.coefficients %>% dim()
+model_coefs <- model_alshaya$dynamic.regression.coefficients %>% 
+  apply(2:3, mean) %>% t
+model_coefs[, "facebook_impressions_h_as"] %>% plot(type = "l")
+model_coefs[, "non_brand_search_impressions_h_as"] %>% plot(type = "l")
+model_coefs[, "criteo_impressions_h_as"] %>% plot(type = "l")
+model_coefs[, "gdn_impressions_h_as"] %>% plot(type = "l")
+model_coefs[, "snapchat_impressions_h_as"] %>% plot(type = "l")
+model_coefs[, "online_promo_discount_on"] %>% plot(type = "l")
+model_coefs[, "online_promo_sale_on"] %>% plot(type = "l")
+model_coefs[, "ramadan_on"] %>% plot(type = "l")
+
+reg_contr_array <- array(dim = c(319,8, 0))
+for(i in 1:1000) {
+  data_predictors <- op_base_online %>% dplyr::select(all_of(VARS_MODEL))
+  reg_contr_array <- (data_predictors * 
+    t(model_alshaya$dynamic.regression.coefficients[i,,])) %>% 
+    abind::abind(reg_contr_array, along = 3)
+}
+
+reg_contr_array %>% apply(1:2, mean) %>% rowSums() %>% plot(type = "l")
+
+model_alshaya$state.contributions %>% 
+  apply(2:3, mean) %>% 
+  tail() %>% 
+  t %>% 
+  tail()
+reg_contr_array %>% apply(1:2, mean) %>% rowSums() %>% tail()
+
+all_contr <- abind::abind(model_alshaya$state.contributions, along = 2)
+
+(all_contr %>% apply(2:3, mean) %>% t)[, "regression"]  %>% plot(type = "l")
