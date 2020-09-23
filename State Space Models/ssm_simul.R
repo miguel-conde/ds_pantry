8613a1426ba75d62f566a5850a67f3edb5bd2918
miguelco2000 <miguelco2000@gmail.com>
2020-09-23 07:01:25 +0200

    llt function ssm_simul.R

--- State Space Models/ssm_simul.R
+++ State Space Models/ssm_simul.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 
 ## LLT
 ## 
@@ -20,3 +20,42 @@ mu_t_1 <- (c(mu_0, rep(0, N-1) + delta_t_1 + eta_mu_t) %>% cumsum())
 y_t_1 = mu_t_1 + epsilon_t
 
 plot(y_t_1, type = "l")
+
+llt <- function(N = 1000, 
+                eta_delta_mn = 0, eta_delta_sigma = 1, 
+                eta_mu_sigma = 1000, 
+                epsilon_sigma = 10, 
+                mu_0 = 10,
+                delta_0 = 1, 
+                seed = 2020) {
+  set.seed(seed)
+  # browser()
+  epsilon_t <- rnorm(N, 0, epsilon_sigma)
+  eta_mu_t <- rnorm(N, 0, eta_mu_sigma)
+  eta_delta_t <- rnorm(N, eta_delta_mn, eta_delta_sigma)
+  
+  delta_t_1 <- (c(delta_0, rep(0, N-1)) + eta_delta_t) %>% cumsum()
+  
+  mu_t_1 <- (c(mu_0, rep(0, N-1)) + delta_t_1 + eta_mu_t) %>% cumsum()
+  
+  y_t_1 = mu_t_1 + epsilon_t
+  
+  y_t_1
+
+}
+
+
+plot(llt(), type = "l")
+
+library(manipulate)
+manipulate(plot(llt(eta_delta_mn = mn_delta,
+                    eta_delta_sigma = s_delta,
+                    eta_mu_sigma = s_mu,
+                    epsilon_sigma = s_epsilon,
+                    mu_0 = lvl_0), type = "l"),
+           mn_delta = slider(-1, 1, 0, step = .1),
+           s_delta = slider(0, 10, 1, step = .1),
+           s_mu = slider(0, 2000, 1000, step = 100),
+           s_epsilon = slider(0, 20, 10, step = 1),
+           lvl_0 = slider(-100, 100, 0, step = 10),
+           slope_0 = slider(-1, 1, 0, step = .1))
