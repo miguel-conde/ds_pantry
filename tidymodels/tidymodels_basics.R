91314fcb7f0abe462dd2d822cf9a1da8ff5fbea8
ES\mcondedesimon <mcondedesimon@deloittedigital.es>
2021-05-27 13:37:52 +0200

    resampling 4 evaluating performance

--- tidymodels/tidymodels_basics.R
+++ tidymodels/tidymodels_basics.R
@@ -655,8 +655,8 @@ map_dfr(time_slices$splits, ~ assessment(.x) %>% data_range())
 # Estimating Performance --------------------------------------------------
 
 # Let’s reconsider the previous random forest model contained in the rf_wflow 
-# object. The fit_resamples() function is analogous to fit(), but instead of 
-# having a data argument, fit_resamples() has resamples which expects an rset 
+# object. The tune::fit_resamples() function is analogous to fit(), but instead  
+# of having a data argument, fit_resamples() has resamples which expects an rset 
 # object like the ones shown above. 
 
 # For our example, let’s save the predictions in order to visualize the model 
@@ -668,4 +668,167 @@ set.seed(130)
 rf_res <- 
   rf_wflow %>% 
   fit_resamples(resamples = ames_folds, control = keep_pred)
+
 rf_res
+
+collect_metrics(rf_res)
+
+collect_metrics(rf_res, summarize = FALSE)
+
+# To obtain the assessment set predictions
+assess_res <- collect_predictions(rf_res)
+assess_res
+
+# Let’s compare the observed and predicted values
+assess_res %>% 
+  ggplot(aes(x = Sale_Price, y = .pred)) + 
+  geom_point(alpha = .15) +
+  geom_abline(col = "red") + 
+  coord_obs_pred() + 
+  ylab("Predicted")
+
+over_predicted <- 
+  assess_res %>% 
+  mutate(residual = Sale_Price - .pred) %>% 
+  arrange(desc(abs(residual))) %>% 
+  slice(1)
+over_predicted
+
+ames_train %>% 
+  slice(over_predicted$.row) %>% 
+  select(Gr_Liv_Area, Neighborhood, Year_Built, Bedroom_AbvGr, Full_Bath)
+
+
+# Parallel Processing -----------------------------------------------------
+
+# The tune package uses the foreach package to facilitate parallel computations. 
+# These computations could be split across processors on the same computer or 
+# across different computers, depending on the chosen technology.
+
+# For computations conducted on a single computer, the number of possible 
+# “worker processes” is determined by the parallel package:
+
+# The number of physical cores in the hardware:
+parallel::detectCores(logical = FALSE)
+
+# The number of possible independent processes that can 
+# be simultaneously used:  
+parallel::detectCores(logical = TRUE)
+
+# The difference between these two values is related to the computer’s processor. 
+# For example, most Intel processors use hyper-threading which creates two 
+# virtual cores for each physical core. While these extra resources can improve 
+# performance, most of the speed-ups produced by parallel processing occur when 
+# processing uses fewer than the number of physical cores.
+
+# Unix and macOS only
+# library(doMC)
+# registerDoMC(cores = 2)
+
+# Now run fit_resamples()...
+
+# To reset the computations to sequential processing:
+# registerDoSEQ(
+
+# All operating systems
+library(doParallel)
+
+# Create a cluster object and then register: 
+cl <- makePSOCKcluster(2)
+registerDoParallel(cl)
+
+# Now run fit_resamples()`...
+
+stopCluster(cl)
+
+
+# Saving the resampled objects --------------------------------------------
+
+ames_rec <- 
+  recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type + 
+           Latitude + Longitude, data = ames_train) %>%
+  step_other(Neighborhood, threshold = 0.01) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_interact( ~ Gr_Liv_Area:starts_with("Bldg_Type_") ) %>% 
+  step_ns(Latitude, Longitude, deg_free = 20)
+
+lm_wflow <-  
+  workflow() %>% 
+  add_recipe(ames_rec) %>% 
+  add_model(linear_reg() %>% set_engine("lm")) 
+
+lm_fit <- lm_wflow %>% fit(data = ames_train)
+
+# Select the recipe: 
+pull_workflow_prepped_recipe(lm_fit)
+
+get_model <- function(x) {
+  pull_workflow_fit(x) %>% tidy()
+}
+
+# Test it using: 
+# get_model(lm_fit)
+
+ctrl <- control_resamples(extract = get_model)
+
+lm_res <- lm_wflow %>%  fit_resamples(resamples = ames_folds, control = ctrl)
+lm_res
+
+lm_res$.extracts[[1]]
+
+# To get the results
+lm_res$.extracts[[1]][[1]]
+
+all_coef <- map_dfr(lm_res$.extracts, ~ .x[[1]][[1]])
+# Show the replicates for a single predictor:
+filter(all_coef, term == "Year_Built")
+
+
+# Summary code ------------------------------------------------------------
+
+library(tidymodels)
+data(ames)
+ames <- mutate(ames, Sale_Price = log10(Sale_Price))
+
+set.seed(123)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+ames_rec <- 
+  recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type + 
+           Latitude + Longitude, data = ames_train) %>%
+  step_log(Gr_Liv_Area, base = 10) %>% 
+  step_other(Neighborhood, threshold = 0.01) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_interact( ~ Gr_Liv_Area:starts_with("Bldg_Type_") ) %>% 
+  step_ns(Latitude, Longitude, deg_free = 20)
+
+lm_model <- linear_reg() %>% set_engine("lm")
+
+lm_wflow <- 
+  workflow() %>% 
+  add_model(lm_model) %>% 
+  add_recipe(ames_rec)
+
+lm_fit <- fit(lm_wflow, ames_train)
+
+rf_model <- 
+  rand_forest(trees = 1000) %>% 
+  set_engine("ranger") %>% 
+  set_mode("regression")
+
+rf_wflow <- 
+  workflow() %>% 
+  add_formula(
+    Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type + 
+      Latitude + Longitude) %>% 
+  add_model(rf_model) 
+
+set.seed(55)
+ames_folds <- vfold_cv(ames_train, v = 10)
+
+keep_pred <- control_resamples(save_pred = TRUE, save_workflow = TRUE)
+
+set.seed(130)
+rf_res <- rf_wflow %>% fit_resamples(resamples = ames_folds, control = keep_pred)
