09ca4c8b1faa0c616720397d35b8d05b2ea4e0ce
miguelco2000 <miguelco2000@gmail.com>
2021-09-15 07:43:41 +0200

    fundamentos_inf_bayesiana.R: muestreo de la posterior

--- rethinking/fundamentos_inf_bayesiana.R
+++ rethinking/fundamentos_inf_bayesiana.R
@@ -152,9 +152,9 @@ model {
 
 fit2 <- stan(model_code = stan_code2, 
             iter = 1000, chains = 1, 
-            data = list(N = n_bernouillis_x_obs, 
-                        N_obs = n_obs,
-                        y = dato_num_azules),
+            data = list(N = n_bernouillis_x_obs2, 
+                        N_obs = n_obs2,
+                        y = dato_num_azules2),
             verbose = TRUE)
 
 fit2
@@ -241,6 +241,8 @@ hist(stan_posterior3)
 
 # 4 - SAMPLING ------------------------------------------------------------
 
+# 4.1 - Muestreo de la posterior ------------------------------------------
+
 ## Del modelo 2
 ## 10000 muestras de la posterior:
 post_samples_2 <- sample(x = ps_grid2, 
@@ -249,3 +251,132 @@ post_samples_2 <- sample(x = ps_grid2,
                          replace = TRUE)
 plot(post_samples_2)
 density(post_samples_2) %>% plot()
+
+# 4.2 - Aplicaciones del muestreo de la posterior -------------------------
+
+
+# 4.2.1 - Probabilidad de que un parámetro esté acotado entre límites -----
+
+# Probabilidad posterior de que p < 0.75
+sum(posterior2[ps_grid2 < 0.75]) # Exacta, casi nunca la podremos calcular
+mean(post_samples_2 < 0.75)      # De la posterior, casi igual
+mean(rstan::extract(fit2)$p < 0.75)
+
+# Probabilidad posterior de que p < 0.7 y p > 0.6
+sum(posterior2[ps_grid2 < 0.7 & ps_grid2 > 0.6]) # Exacta, casi nunca la podremos calcular
+mean(post_samples_2 < 0.7 & post_samples_2 > 0.6)      # De la posterior, casi igual
+mean(rstan::extract(fit2)$p < 0.7 & 
+       rstan::extract(fit2)$p > 0.6)
+
+
+# 4.2.2 - Intervalos de masa definida - Intervalos de credibilidad --------
+
+# Entre qué 2 valores de p se encuentra el 89% de la probabilidad
+quantile(post_samples_2, probs = c((1-.89)/2, 1-(1-.89)/2))
+quantile(rstan::extract(fit2)$p, probs = c((1-.89)/2, 1-(1-.89)/2))
+
+# Este tipo de intervalos asignan la misma masa de probailiadd a cada cola.
+# Se llaman "PERCENTILE INTERVALS", PI.
+quantile(post_samples_2, probs = c(0.1, 1 - 0.1))
+quantile(post_samples_2, probs = c(0.2, 1 - 0.2))
+quantile(post_samples_2, probs = c(0.3, 1 - 0.3))
+quantile(post_samples_2, probs = c(0.25, 1 - 0.25))
+
+# Funcionan bien si la posterior es bastante simétrica. Si está bastante sesgada,
+# mjor usar HISGHEST POSTERIOR DENSITY INTERVALS (HDPI) = el intervalo más estrecho
+# que contiene la masa de probabilidad especificada
+rethinking::HPDI(post_samples_2, prob = 0.5)
+
+
+# 4.2.3 - Estimaciones puntuales ------------------------------------------
+
+## Sirven como resúmenes de la posterior
+
+## 1 - Estimación MAP (Maximum A Posteriori) - MODA
+ps_grid2[which.max(posterior2)] # Exacta, no siempre podremos calcularla
+rethinking::chainmode(post_samples_2, adj = 0.01)
+rethinking::chainmode(rstan::extract(fit2)$p, adj = 0.01)
+
+## 2 - Media
+sum(ps_grid2 * posterior2)
+mean(post_samples_2)
+mean(rstan::extract(fit2)$p)
+
+## 3 - Mediana
+quantile(post_samples_2, prob = .5)
+  quantile(rstan::extract(fit2)$p, prob = .5)
+
+## 4 - Function loss
+
+# Minimizamos una medida de la incertidumbre sobre el valor real
+
+## L1 - Coincide con la MEDIANA
+loss <-sapply(ps_grid2, function(d) sum(posterior2 * abs(d - ps_grid2)))
+ps_grid2[which.min(loss)]
+
+## L2 - Coincide con la media
+loss <-sapply(ps_grid2, function(d) sum(posterior2 * (d - ps_grid2)^2))
+ps_grid2[which.min(loss)]
+
+
+# 4.3 - Muestreo para simular predicciones --------------------------------
+
+
+# 4.3.1 - Dummy data ------------------------------------------------------
+
+# Es lo que hemos hecho para los datos de los modelos de arriba
+
+# Comprobación:
+# Probabilidades de cada caso:
+dbinom(0:n_bernouillis_x_obs2, size = n_bernouillis_x_obs2, prob = p)
+
+# Simulamos dummy data
+dummy_data <- rbinom(n = 1e6, size = n_bernouillis_x_obs2, prob = p)
+table(dummy_data) / 1e6
+
+
+# 4.3.2 - Model Checking --------------------------------------------------
+
+
+# 4.3.2.1 - Posterior Predictive Distribution -----------------------------
+
+## PPS (Posterior Predictive Sampling)
+w <- rbinom(1e5, size = n_bernouillis_x_obs2, prob = post_samples_2)
+
+# Con Stan
+stan_code2_pps <- "
+data {
+  int<lower=0> N;
+  int<lower=0> N_obs;
+  int y[N_obs];
+}
+
+parameters {
+  real<lower=0, upper=1> p;
+}
+
+model {
+  p ~ uniform(0, 1); 
+  y ~ binomial(N, p);
+}
+
+generated quantities {
+  int y_pps[N_obs];
+  
+  for (n in 1:N_obs) {
+    y_pps[n] = binomial_rng(N, p);
+  }
+    
+}
+"
+
+fit2_pps <- stan(model_code = stan_code2_pps, 
+             iter = 1000, chains = 1, 
+             data = list(N = n_bernouillis_x_obs2, 
+                         N_obs = n_obs2,
+                         y = dato_num_azules2),
+             verbose = TRUE)
+
+# Salen 500 (1 por cada post-warmup draws) distribuciones de cada simulación predictiva
+
+extract(fit2_pps)$y_pps %>% apply(1, function(x) table(x)/100) %>% bind_rows()
