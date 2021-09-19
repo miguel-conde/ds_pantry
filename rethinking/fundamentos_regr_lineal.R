b64a8546a65da24fd201f543382a3ab7bc5b52ea
miguelco2000 <miguelco2000@gmail.com>
2021-09-19 09:24:43 +0200

    fundamentos_regr_lineal.R hasta 4.5

--- rethinking/fundamentos_regr_lineal.R
+++ rethinking/fundamentos_regr_lineal.R
@@ -5,8 +5,511 @@ library(rstan)
 library(rethinking)
 
 
+# DATA --------------------------------------------------------------------
+
+
 data(Howell1)
 
 d <- Howell1
+d2 <- Howell1 %>% filter(age >= 18)
 
 
+# HEIGHT MODEL ------------------------------------------------------------
+
+
+stan_code <- "
+data{
+  int<lower=0> N;
+  vector[N] h;
+}
+
+parameters {
+  real mu;
+  real<lower=0,upper=50> sigma;
+}
+
+model {
+  mu ~ normal(180, 20); // 95% mu in 140 - 220
+  sigma ~ uniform(0, 50); // 95 % h in mu +/- 0 - 100
+  h ~ normal(mu, sigma); 
+}
+
+"
+
+# 1.Prior exploration - Prior predictive ----------------------------------
+
+# Explore priors
+curve(dnorm(x, 180, 20), from = 100, to = 240, main = "Prior mu")
+curve(dunif(x, 0, 50), from = -100, to = 100, main = "Prior sigma")
+
+# Prior predictive simulation
+prior_sample_mu <- rnorm(1e4, 180, 20)
+prior_sample_sigma <- runif(1e4, 0, 50)
+prior_sample_h <- rnorm(1e4, prior_sample_mu, prior_sample_sigma)
+
+plot(density(prior_sample_h))
+
+# What if greater sigma prior?
+prior_sample_h2 <- rnorm(1e4, prior_sample_mu, runif(1e4, 0, 100))
+lines(density(prior_sample_h2), col = "red")
+
+sum(prior_sample_h2 < 0) / 1e4   # Not credible
+sum(prior_sample_h2 > 272) / 1e4 # Not credible
+
+
+# 2.Grid approximation ----------------------------------------------------
+
+post_table <- expand.grid(mu = seq(150, 160, length.out = 100),
+                 sigma = seq(7, 9, length.out = 100))
+
+# Log-likelihood
+post_table$LL <- sapply(1:nrow(post_table),
+                        function(i) {
+                          sum(
+                            dnorm(d2$height, post_table$mu[i], post_table$sigma[i],
+                                  log = TRUE)
+                          ) 
+                        }) 
+
+post_table$log_posterior <- post_table$LL +
+  dnorm(post_table$mu, 180, 20, TRUE) +
+  dunif(post_table$sigma, 0, 50, TRUE)
+
+# Finally,the obstacle for getting back on the probability scale is that 
+# rounding error is always a threat when moving from log-probability to 
+# probability. If you use the obvious approach, like exp(post$prod), you’ll
+# get a vector full of zeros, which isn’t very helpful.This is a result of 
+# R’s rounding very small probabilities to zero.Remember,in large samples, all
+# unique samples are unlikely.This is why you have to work with log-probability.
+# The code in the box dodges this problem by scaling all of the log-products
+# by the maximum log-product.As a result, the values in post$prob are not all 
+# zero, but the yals oaren’t exactly probabilities.Instead they are relative 
+# posterior probabilities.But that’s good enough for what we wish to do with 
+# these values.
+
+post_table$posterior <- exp(post_table$log_posterior - max(post_table$log_posterior))
+
+
+rethinking::contour_xyz(post_table$mu, post_table$sigma, post_table$posterior)
+rethinking::image_xyz(post_table$mu, post_table$sigma, post_table$posterior)
+
+
+# 2.1 Sampling from the posterior ------------------------------------------
+
+sample_rows <- sample(1:nrow(post_table), size = 1e4, 
+                      prob = post_table$posterior, replace = TRUE)
+sample_mu <- post_table$mu[sample_rows]
+sample_sigma <- post_table$sigma[sample_rows]
+
+plot(sample_mu, sample_sigma,
+     cex = 0.5, pch = 16, col = rethinking::col.alpha(rangi2,0.1))
+
+density(sample_mu) %>% plot()
+density(sample_sigma) %>% plot()
+
+PI(sample_mu)
+PI(sample_sigma)
+
+
+# 2.2 Posterior predictive Sampling (PPS) ----------------------------------
+
+pps_sample <- rnorm(1e4, sample_mu, sample_sigma)
+
+sum(pps_sample < 0) / 1e4   # Quite credible
+sum(pps_sample > 272) / 1e4 # Quite credible
+
+
+# 3. Stan model -----------------------------------------------------------
+
+options(mc.cores = parallel:: detectCores())
+rstan_options(auto_write = TRUE)
+
+fit <- stan(model_code = stan_code, 
+            iter = 1000, chains = 1, 
+            data = list(N = length(d2$height), h = d2$height),
+            verbose = TRUE)
+
+fit
+precis(fit)
+
+
+# 3.1 Sampling from the posterior -----------------------------------------
+
+sample_mu <- rstan::extract(fit, "mu")[[1]]
+sample_sigma <- rstan::extract(fit, "sigma")[[1]]
+
+pairs(fit)
+
+density(sample_mu) %>% plot()
+density(sample_sigma) %>% plot()
+
+PI(sample_mu)
+PI(sample_sigma)
+
+# 3.2 - Posterior predictive sampling - PPS -------------------------------
+
+stan_code_pps <- "
+data{
+  int<lower=0> N;
+  vector[N] h;
+}
+
+parameters {
+  real mu;
+  real<lower=0,upper=50> sigma;
+}
+
+model {
+  mu ~ normal(180, 20); // 95% mu in 140 - 220
+  sigma ~ uniform(0, 50); // 95 % h in mu +/- 0 - 100
+  h ~ normal(mu, sigma); 
+}
+
+generated quantities {
+  vector[N] h_sim;
+  
+  for (i in 1:N) {
+    h_sim[i] = normal_rng(mu, sigma);
+  }
+}
+
+"
+
+fit_pps <- stan(model_code = stan_code_pps, 
+            iter = 1000, chains = 1, 
+            data = list(N = length(d2$height), h = d2$height),
+            verbose = TRUE)
+
+fit_pps
+precis(fit_pps)
+
+pps_samples <- extract(fit_pps)$h_sim 
+
+# También:
+smaple_mu <- sample(extract(fit_pps)$mu, size = 1e4, replace = TRUE)
+sample_sigma <- sample(extract(fit_pps)$sigma, size = 1e4, replace = TRUE)
+
+pps_sample2 <- rnorm(1e4, sample_mu, sample_sigma)
+
+sum(pps_samples[1, ] < 0) / 1e4   # Quite credible
+sum(pps_samples[1, ] > 272) / 1e4 # Quite credible
+
+
+plot(density(d2$height), ylim = c(0, 0.06), lwd = 2)
+
+for (i in sample(1:nrow(pps_samples), size = 20)) {
+  lines(density(pps_samples[i, ]), col = "red", lty = 2)
+}
+
+lines(density(pps_sample2), col = "blue", lty = 1, lwd = 2)
+
+# Sampling from posterior
+precis(extract(fit_pps), hist = FALSE)
+
+# Posterior predictive sampling
+precis(pps_samples[1, ], hist = FALSE)
+precis(pps_samples[10, ], hist = FALSE)
+precis(pps_samples[100, ], hist = FALSE)
+precis(pps_sample2, hist = FALSE)
+
+
+# LINEAR PREDICTION -------------------------------------------------------
+
+ggplot(d2, aes(x = weight, y = height)) + geom_point()
+
+
+stan_code_linear <- "
+data {
+  int<lower=0> N;
+  
+  vector [N] weights;
+  vector [N] heights;
+  real weights_bar;
+}
+
+parameters {
+  real<lower=0, upper=50> sigma;
+  real<lower=0> beta;
+  real<lower=0> alpha;
+}
+
+transformed parameters {
+  real<lower=0> mu[N];
+  
+  for ( i in 1:N) {
+    mu[i] = beta * (weights[i] - weights_bar) + alpha;
+  }
+}
+
+model {
+
+  sigma ~ uniform(0, 50);
+  beta ~ lognormal(0, 1);
+  alpha ~ normal(180, 20); // Como la mu del modelo anterior
+                           // ya que alfa es el valor que toma mu
+                           // cuando el peso es el peso medio
+  heights ~ normal(mu, sigma);
+
+}
+
+generated quantities {
+  real mu_sim[N];
+  vector[N] heights_sim;
+ 
+  for (i in 1:N) {
+    // POSTERIOR PREDICTIVE SAMPLING (PPS)
+    
+    // mu PPS
+    mu_sim[i] = beta * (weights[i] - weights_bar) + alpha;
+    
+    // heights PPS
+    heights_sim[i] = normal_rng(mu[i], sigma);
+  }
+}
+
+"
+
+# 1. Priors exploration ---------------------------------------------------
+
+## Prior Predictive Distribution
+set.seed(2971)
+N <- 100
+a <- rnorm(N, 180, 20)
+b <- rnorm(N, 0, 10)
+
+plot(NULL,
+     xlim = range(d2$weight),
+     ylim = c(-100, 400),
+     xlab = "weight",
+     ylab = "height")
+abline(h = 0, lty = 2)
+abline(h = 272, lty = 1, lwd =0.5)
+mtext("b ~ dnorm(0, 10)")
+
+x_bar <- mean(d2$weight)
+
+for (i in 1:N) {
+  curve(a[i] + b[i] * (x - x_bar),
+        from = min(d2$weight),
+        to = max(d2$weight),
+        add = TRUE,
+        col = col.alpha("black", 0.2))
+}
+
+# Con esta prior la relación entre peso y altura puede ser positiva o negativa
+# e incluso a algunos pesos les corresponden alturas negativas o superiores
+# a la del hombre más alto del mundo.
+# Mejor usar una log-normal:
+
+b <- rlnorm(N, 0, 1)
+density(b) %>% plot()
+
+plot(NULL,
+     xlim = range(d2$weight),
+     ylim = c(-100, 400),
+     xlab = "weight",
+     ylab = "height")
+abline(h = 0, lty = 2)
+abline(h = 272, lty = 1, lwd =0.5)
+mtext("b ~ dnorm(0, 10)")
+
+x_bar <- mean(d2$weight)
+
+for (i in 1:N) {
+  curve(a[i] + b[i] * (x - x_bar),
+        from = min(d2$weight),
+        to = max(d2$weight),
+        add = TRUE,
+        col = col.alpha("black", 0.2))
+}
+
+# Mucho mejor esta prior
+
+
+# 2. Ulam model -----------------------------------------------------------
+
+ulam_linear <- ulam(
+  alist(
+    height ~ dnorm(mu, sigma),
+    mu <- a + b * (weight - xbar),
+    a ~ dnorm(178, 20),
+    b ~ dlnorm(0, 1),
+    sigma ~ dunif(0, 50)
+  ), data = d2 %>% mutate(xbar = x_bar))
+
+ulam_linear
+precis(ulam_linear)
+
+# 3. Stan model -----------------------------------------------------------
+
+
+fit_linear <- stan(model_code = stan_code_linear, 
+            iter = 1000, chains = 1, 
+            data = list(N = length(d2$height), 
+                        heights = d2$height, 
+                        weights_bar = mean(d2$weight),
+                        weights = d2$weight),
+            verbose = TRUE)
+
+print(fit_linear, pars = c("alpha", "beta", "sigma"))
+precis(fit_linear)
+
+
+# UNDERSTANDING THE POSTERIOR ---------------------------------------------
+
+# I emphasize plotting posterior distributions and posterior preictions,
+# instead of attempting to understand a table.
+
+
+# 1. Tablas de DISTRIBUCIONES MARGINALES ----------------------------------
+
+precis(fit_linear)
+
+# Beta = 0.91 => cada kilo más / menos, 1 cm más / menos
+# 89% de la probabilidad muy concentrado OK
+
+vcov(ulam_linear)
+
+cov(data.frame(alpha = extract(fit_linear, "alpha"), 
+               beta = extract(fit_linear, "beta"), 
+               sigma = extract(fit_linear, "sigma")))
+
+# Very little covariation among the parameters in this case.
+
+pairs(ulam_linear)
+pairs(fit_linear, pars = c("alpha", "beta", "sigma"))
+
+# The lack of covariance among the parameters results from centering
+
+
+# 2. Plotting posterior inference vs data ---------------------------------
+
+# It’s almost always much more useful to plot the posteriorinference against
+# the data.Not only does plotting help in interpretng the posterior, but it 
+# also provides an informal check on model assumptions. Whent he model’s
+# predictions don’t come close to key observations or patterns in the plotted
+# data, then you might suspect the model either did not fit correctly or is 
+# rather badly specified.
+
+## Versión 1 - supersimple
+post_samples <- extract(fit_linear, pars = c("alpha", "beta", "sigma"))
+a_map <- mean(post_samples$alpha)
+b_map <- mean(post_samples$beta)
+
+plot(height ~ weight, d2, col = rangi2)
+curve(a_map + b_map * (x - x_bar), add = TRUE)
+
+
+# a. Visualizar la incertidumbre ------------------------------------------
+
+
+
+for (i in sample(length(post_samples$alpha), size = 100)) {
+  curve(post_samples$alpha[i] + post_samples$beta[i] * (x - x_bar), 
+        col = col.alpha("black", 0.3), 
+        add = TRUE)
+}
+
+
+# b. Regression intervals and contours ------------------------------------
+
+mu_at_50 <- post_samples$alpha + post_samples$beta * (50 - x_bar)
+dens(mu_at_50,col = rangi2, lwd = 2, xlab = "mu|weight=50")
+
+PI(mu_at_50, prob = 0.89)
+
+# Para todos los valores de weight - LINK function:
+mu <- rethinking::link(ulam_linear)
+str(mu)
+
+# Igual que:
+extract(fit_linear, "mu_sim")[[1]] %>% str()
+
+# O:
+sapply(d2$weight, 
+       function(x) post_samples$alpha + post_samples$beta * (x - x_bar)) %>% 
+  str()
+
+# Si queremos más:
+sapply(d2$weight, 
+       function(x) sample(post_samples$alpha, size = 1000, replace = TRUE) + 
+         sample(post_samples$beta, size = 1000, replace = TRUE) * (x - x_bar)) %>% 
+  str()
+
+# Es decir, cada columna es una muestra de la distribución posterior de cada 
+# caso (352 individuos)
+
+## Calculemos lo mismo pero no para los weights en d2, sino para los que 
+# queramos:
+weight_seq <- seq(25, 70, 1)
+
+mu <- link(ulam_linear, data = data.frame(weight = weight_seq, xbar = x_bar))
+mu %>% str()
+
+# O:
+sapply(weight_seq, 
+       function(x) post_samples$alpha + post_samples$beta * (x - x_bar)) %>% 
+  str()
+
+# Y ahora pintamos:
+plot(height ~ weight, d2, type = "n")
+
+for(i in 1:nrow(mu)) {
+  points(weight_seq, mu[i,], pch = 16, col = col.alpha(rangi2, 0.1))
+}
+
+# Cada apilamiento es una distribución posterior gaussiana. Hagamos sus 
+# summaries:
+mu_mean <- apply(mu, 2, mean)
+mu_PIs <- apply(mu, 2, PI, 0.89)
+mu_HPDIs <- apply(mu, 2, HPDI, 0.89)
+
+# Y pintémoslo:
+plot(height ~ weight, d2, col = col.alpha(rangi2, 0.5))
+lines(weight_seq, mu_mean)
+shade(mu_PIs, weight_seq, col = col.alpha("gray", 1))
+
+
+# c. Intervalos de predicción ---------------------------------------------
+
+post_sample_alpha <- extract(fit_linear)$alpha
+post_sample_beta <- extract(fit_linear)$beta
+post_sample_sigma <- extract(fit_linear)$sigma
+
+sim_heights <- sapply(weight_seq,
+                      function(x) {
+                        
+                        post_sample_mu <- post_sample_beta * (x - x_bar) + 
+                          post_sample_alpha
+                        
+                        rnorm(length(post_sample_mu), 
+                              post_sample_mu, 
+                              post_sample_sigma)
+                      })
+
+sim_heights_means <- apply(sim_heights, 2, mean)
+sim_heights_PIs <- apply(sim_heights, 2, PI, 0.89)
+sim_heights_HPDIs <- apply(sim_heights, 2, HPDI, 0.89)
+
+# Igual que:
+sim(ulam_linear, data = list(weight = weight_seq, 
+                             xbar = rep(x_bar, length(weight_seq))))
+
+# O:
+extract(fit_linear, "heights_sim")[[1]]
+
+# (aunque esto no es con weight_seq)
+
+# Plot de todo
+
+# 1 - Los datos
+plot(height ~ weight, d2, col = col.alpha(rangi2, 0.5))
+
+# 2 - La línea de regresión media (MAP line)
+lines(weight_seq, mu_mean)
+
+# 3 - Región HPDI de la MAP line
+shade(mu_HPDIs, weight_seq, col = col.alpha("blue", alpha = 1))
+
+# 4 - Región PI de las predicciones o simulaciones
+shade(sim_heights_PIs, weight_seq, col = col.alpha("grey", alpha = 1))
