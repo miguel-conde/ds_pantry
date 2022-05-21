464b1371d95494a1900eaa21bb5cd82b2e42dd6b
miguelco2000 <miguelco2000@gmail.com>
2022-05-21 08:26:54 +0200

    Limpieza de todo lo bayesiano

---
+++ dates_and_times/dates_and_times.R
@@ -0,0 +1,6 @@
+# Data Wrangling in R - Chapter 9 Date & Time 
+#   - https://wrangle-r.rsquaredacademy.com/date-and-time-in-r.html
+# R for Data Science - 16 Dates and Times
+#   - lubridate: https://r4ds.had.co.nz/dates-and-times.html
+# Comprehensive Date-Time Handling for R
+#   - clock: https://www.tidyverse.org/blog/2021/03/clock-0-1-0/
