10763a7087de9978d43a19ace42fc8737763aab6
mcondedesimon <mcondedesimon@deloitte.es>
2022-12-01 16:09:09 +0100

    + contribs lmer

--- Hierarchichal Models/contribs_lmer.R
+++ Hierarchichal Models/contribs_lmer.R
@@ -149,7 +149,87 @@ get_mlmer_contribs <- function(in_model, new_data = NULL, pred = FALSE, ...) {
   return(out)
 }
 
-calc_contribs <- function(m_h_lm, base_emisiones) {
+####
+
+get_mlmer_contribs <- function(in_model, new_data = NULL, pred = FALSE, ...) {
+  
+  # in_model
+  # a fitted model object
+  #
+  # new_data
+  # data frame for which to evaluate predictions.
+  #
+  # pred
+  # (logical) add a column with the sum of the contributions (i.e., the 
+  # prediction)?
+  #
+  # ...
+  #    re.form	
+  #    (formula, NULL, or NA) specify which random effects to condition on when 
+  #    predicting. If NULL, include all random effects; if NA or ~0, include no 
+  #    random effects.
+  #   
+  #    random.only	
+  #    (logical) ignore fixed effects, making predictions only using random 
+  #    effects?
+  
+  if (is.null(new_data)) new_data <- in_model@frame
+  
+  model_vars <- coef(in_model) %>% 
+    lapply(names) %>% 
+    unlist() %>% 
+    str_split(":") %>% 
+    unlist() %>% 
+    unique()
+  
+  has_intcpt <- "(Intercept)" %in% model_vars
+  names_vars <- setdiff(model_vars, "(Intercept)")
+  out_length <- length(names_vars) 
+  
+  orig_pred <- predict(in_model, newdata = new_data, ...)
+  
+  if (has_intcpt == TRUE) {
+    
+    aux <- new_data %>% mutate_if(is.numeric, ~ 0) 
+    out <- tibble(`(Intercept)` = predict(in_model, newdata = aux, ...))
+    
+    if (out_length > 0) {
+      
+      for (v in names_vars) {
+        aux <- new_data %>% mutate(!!sym(v) := 0)
+        out <- out %>% mutate(!!sym(v) := orig_pred - predict(in_model, newdata = aux, ...))
+      }
+    }
+  } else {
+    
+    out <- vector(mode = "list", length = out_length)
+    
+    if (out_length > 0) {
+      for (v in names_vars) {
+        
+        aux <- new_data %>% mutate(!!sym(v) := 0)
+        out[[v]] = orig_pred - predict(in_model, newdata = aux, ...)
+      }
+      
+      out <- bind_cols(out)
+    }
+  }
+  
+  model_vars <- model_vars %>% intersect(c(names(new_data), "(Intercept)"))
+  out <- out %>% select(all_of(model_vars))
+  
+  if (pred == TRUE) {
+    
+    out <- out %>% 
+      rowwise() %>% 
+      mutate(pred = sum(c_across())) %>% 
+      ungroup()
+  }
+  
+  return(out)
+}
+
+get_lme4_str_formulas <- function(m_h_lm) {
   
   str_f <- paste0(format(summary(m_h_lm)$call$formula), collapse = "")
   
@@ -169,30 +249,61 @@ calc_contribs <- function(m_h_lm, base_emisiones) {
           str_remove("\\(|\\)") %>% 
           str_squish)
   
-  contrib_f <- 
-    as.formula(paste(str_response, "~ ", 
-                     paste(unique(c(unlist(str_split(fe_str_f, "\\+")),
-                                    unlist(str_split(re_str_f, "\\+")))),
-                           collapse = "+")))
+  out <- list(fe_str_f = fe_str_f,
+              re_str_f = re_str_f)
   
-  mm <- model.matrix(contrib_f, base_emisiones)
+  return(out)
+}
+
+calc_contribs <- function(m_h_lm, base) {
+  
+  # str_f <- paste0(format(summary(m_h_lm)$call$formula), collapse = "")
+  # 
+  # str_response <- str_f %>% str_remove("~.*$") %>% str_trim()
+  
+  # fe_str_f <- str_f %>% 
+  #   str_remove("^.*~") %>% 
+  #   str_remove("\\(.*$") %>% 
+  #   str_remove("[:space:]*\\+[:space:]*$") %>% 
+  #   str_squish()
+  # 
+  # 
+  # re_str_f <- str_f %>% 
+  #   str_extract("\\(.*\\)") %>% 
+  #   str_split("\\+[:digit:]*\\(") %>% 
+  #   map(~ .x %>% str_remove("\\|.*\\)") %>% 
+  #         str_remove("\\(|\\)") %>% 
+  #         str_squish)
+  str_ff <- get_lme4_str_formulas(m_h_lm)
+  
+  # contrib_f <- 
+  #   as.formula(paste(str_response, "~ ", 
+  #                    paste(unique(c(unlist(str_split(fe_str_f, "\\+")),
+  #                                   unlist(str_split(re_str_f, "\\+")))),
+  #                          collapse = "+")))
+  
+  model_vars <- unique(unlist(map(str_ff, ~ unlist(str_split(.x, "\\+")))))
+  contrib_f <- 
+    as.formula(paste(str_response, "~ ", paste(model_vars, collapse = "+")))
+  
+  mm <- model.matrix(contrib_f, base)
   mm <- mm[, order(colnames(mm))]
   
-  progs <- unique(base_emisiones$name_edit)
+  progs <- unique(base$name_edit)
   progs <- progs[order(progs)]
   contribs_lst <- vector(mode = "list", length = length(progs))
   names(contribs_lst) <- progs
   
   for (prog in progs) {
     
-    idx_prog <- which(base_emisiones$name_edit == prog)
+    idx_prog <- which(base$name_edit == prog)
     
     m_matrix_prog <- mm[idx_prog, , drop = FALSE]
     coefs_prog <- coef(m_h_lm)$name_edit[prog, colnames(m_matrix_prog)]
     coefs_prog <- coefs_prog[, order(names(coefs_prog))] %>% 
       unlist()
     
-    contribs_lst[[prog]] <- base_emisiones %>% 
+    contribs_lst[[prog]] <- base %>% 
       filter(name_edit == prog) %>% 
       select(date, name_edit, class_ytb) %>% 
       bind_cols(m_matrix_prog %>% sweep(2, coefs_prog, "*")) %>% 
