fa733c3af98fabb9b5482c84e1ccf49fd63ac54e
miguelco2000 <miguelco2000@gmail.com>
2023-05-06 12:43:16 +0200

    Exploring australian tourism

--- Forecast/fpp3/fpp3.R
+++ Forecast/fpp3/fpp3.R
@@ -452,3 +452,47 @@ tourism |>
   ggplot(aes(x = Quarter, y = Trips)) +
   geom_line() +
   facet_grid(vars(State, Region, Purpose))
+
+# Exploring Australian tourism data ---------------------------------------
+
+tourism_features <- tourism |>
+  features(Trips, feature_set(pkgs = "feasts"))
+
+library(glue)
+tourism_features |>
+  select_at(vars(contains("season"), Purpose)) |>
+  mutate(
+    seasonal_peak_year = seasonal_peak_year +
+      4*(seasonal_peak_year==0),
+    seasonal_trough_year = seasonal_trough_year +
+      4*(seasonal_trough_year==0),
+    seasonal_peak_year = glue("Q{seasonal_peak_year}"),
+    seasonal_trough_year = glue("Q{seasonal_trough_year}"),
+  ) |>
+  GGally::ggpairs(mapping = aes(colour = Purpose))
+
+library(broom)
+pcs <- tourism_features |>
+  select(-State, -Region, -Purpose) |>
+  prcomp(scale = TRUE) |>
+  augment(tourism_features)
+pcs |>
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, col = Purpose)) +
+  geom_point() +
+  theme(aspect.ratio = 1)
+
+outliers <- pcs |>
+  filter(.fittedPC1 > 10) |>
+  select(Region, State, Purpose, .fittedPC1, .fittedPC2)
+
+outliers |>
+  left_join(tourism, by = c("State", "Region", "Purpose"), multiple = "all") |>
+  mutate(Series = glue("{State}", "{Region}", "{Purpose}", .sep = "\n\n")) |>
+  ggplot(aes(x = Quarter, y = Trips)) +
+  geom_line() +
+  facet_grid(Series ~ ., scales = "free") +
+  labs(title = "Outlying time series in PC space")
+
+# THE FORECASTER TOOLBOX --------------------------------------------------
+
+
