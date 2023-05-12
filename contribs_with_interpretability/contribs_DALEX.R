0d48c5afc3b7ec621515c10a1943541d9669cf40
mcondedesimon <mcondedesimon@deloitte.es>
2023-05-12 07:58:49 +0200

    contribs DALEX: cosas del paquete rdescr

--- contribs_with_interpretability/contribs_DALEX.R
+++ contribs_with_interpretability/contribs_DALEX.R
@@ -4,6 +4,19 @@
 
 library(tidymodels)
 
+get_mode <- function(v, method = c("first", "all")) {
+  
+  method <- match.arg(method)
+  
+  uniqv <- unique(v)
+  tab <- tabulate(match(v, uniqv))
+  
+  if (method == "first") out <- uniqv[which.max(tab)]
+  else out <- uniqv[tab == max(tab)]
+  
+  return(out)
+}
+
 
 # DATOS -------------------------------------------------------------------
 
@@ -68,7 +81,7 @@ rsq(results_rf, truth = Sepal.Length, estimate = .pred)
 # EXPLICABILIDAD ----------------------------------------------------------
 
 
-# Partial Dependencies ----------------------------------------------------
+# Partial Dependencies - iris ---------------------------------------------
 
 
 
@@ -141,9 +154,7 @@ plot(list(mp_lm, mp_rf))
 
 
 ##
-#  3 TEMAS POR RESOLVER:
 
-# 1 - Pasar de los pdps a contribuciones
 kk_iris_0 <- iris %>% 
   as_tibble() %>% 
   mutate_if(is.numeric, ~ 0) %>% 
@@ -203,82 +214,79 @@ pdp_2_contrib(modelo_pipeline_rf, iris, "Sepal.Width", the_explainer_rf, the_pre
 pdp_2_contrib(modelo_pipeline_lm, iris, "Species", the_explainer_lm, the_pred_function)
 pdp_2_contrib(modelo_pipeline_rf, iris, "Species", the_explainer_lm, the_pred_function)
 
-# 2 - Qué hacer con la intercept
-# 3 - Cómo interpolar
+
+# Cómo interpolar?
+# Baseline / Intercept ?
 
 
-# Código antiguo  ---------------------------------------------------------
+# Partial dependencies - Boston -------------------------------------------
 
+data("Boston", package = "MASS")
 
-the_pred_function2 <- function(object, newdata) {
-  predict(object, newdata) %>% pull(1)
-}
+x_var <- c("lstat", "rm", "dis", "indus")
+y_var <- "medv"
 
-pd_values <- pdp::partial(
-  object = modelo_pipeline_lm,
-  train = iris,
-  pred.var = "Sepal.Width",
-  pred.fun = the_pred_function2, 
-  grid_resolution = 20,
-  ice = TRUE
-)
+# Creamos recipe
+the_recipe <- 
+  recipe(medv ~ lstat + rm + dis + indus, data = Boston) %>%
+  # step_normalize(all_numeric(), -all_outcomes()) %>%
+  # step_pca(all_numeric(), -all_outcomes(), num_comp = 2) %>%
+  step_dummy(all_nominal(), -all_outcomes())
 
-pd_values <- pd_values %>%
-  as_tibble() %>%
-  group_by(Sepal.Width) %>%
-  summarise(yhat = mean(yhat))
+# Creamos el modelo lineal
+the_model_lm <- linear_reg()
 
-pdp_1_contrib <- function(in_model, in_data, pred_var, pred_fun, grid_resolution = 20) {
-  
-  pd_values <- pdp::partial(
-    in_model,
-    train = in_data,
-    pred.var = pred_var,
-    pred.fun = pred_fun,
-    grid.resolution =  grid_resolution,
-    ice = TRUE
-  )
-  
-  aux_class <- class(pd_values)
-  
-  pd_values <- pd_values %>%
-    as_tibble() %>%
-    group_by(!!sym(pred_var)) %>%
-    summarise(yhat = mean(yhat))
-  
-  class(pd_values) <- aux_class
-  
-  avg_y_hat <- pred_fun(in_model, in_data) %>% mean()
-  avg_x <- in_data %>% pull(!!sym(pred_var)) %>% mean()
-  
-  X_aux_0 <- in_data %>% mutate_all(~ 0)
-  # X_aux_0 <- in_data %>% mutate_if(is.numeric, ~ 0) %>% mutate_if(is.factor, ~ levels(.)[1])
-  X_aux_1 <- X_aux_0  %>% mutate(!!sym(pred_var) := avg_x)
-  
-  # La distancia es la media de:
-  #          fitted - fitted cuando todos valen 0 menos el de interés
-  dist <- avg_y_hat - (mean(pred_fun(in_model, X_aux_1)) - mean(pred_fun(in_model, X_aux_0)))
-  
-  # aux_class <- class(pd_values)
-  # out <- pd_values %>%
-  #   as_tibble() %>%
-  #   mutate(yhat = yhat - dist) %>%
-  #   as.data.frame()
-  # class(out) <- aux_class
-  
-  out <- pd_values
-  out$yhat <- out$yhat - dist
-  
-  return(out)
-}
+# Creamos el modelo random forest
+the_model_rf <- rand_forest(engine = "ranger", mode = "regression")
 
-pdp_1_contrib(in_model = modelo_pipeline_lm, 
-              in_data = iris, 
-              pred_var = "Sepal.Width", 
-              pred_fun = the_pred_function2, grid_resolution = 20)
+# Crear un pipeline para preprocesar los datos y ajustar el modelo lineal
+modelo_pipeline_lm <- workflow() %>%
+  add_recipe(the_recipe) %>% 
+  add_model(the_model_lm) %>% 
+  fit(data = Boston)
+
+# Crear un pipeline para preprocesar los datos y ajustar el modelo random forest
+modelo_pipeline_rf <- workflow() %>%
+  add_recipe(the_recipe) %>% 
+  add_model(the_model_rf) %>% 
+  fit(data = Boston)
+
+the_explainer_lm <- DALEX::explain(model = modelo_pipeline_lm,
+                                   data  = Boston %>% select(-medv),
+                                   y     = Boston %>% pull(medv), 
+                                   predict_function = the_pred_function,
+                                   label = "lm")
+
+the_explainer_rf <- DALEX::explain(model = modelo_pipeline_rf,
+                                   data  = Boston %>% select(-medv),
+                                   y     = Boston %>% pull(medv), 
+                                   predict_function = the_pred_function,
+                                   label = "rf")
+
+pdp_2_contrib(modelo_pipeline_lm, Boston, "lstat", the_explainer_lm, the_pred_function)
+pdp_2_contrib(modelo_pipeline_rf, Boston, "lstat", the_explainer_rf, the_pred_function)
+
+pdp_2_contrib(modelo_pipeline_lm, Boston, "rm", the_explainer_lm, the_pred_function)
+pdp_2_contrib(modelo_pipeline_rf, Boston, "rm", the_explainer_rf, the_pred_function)
+
+pdp_2_contrib(modelo_pipeline_lm, Boston, "dis", the_explainer_lm, the_pred_function)
+pdp_2_contrib(modelo_pipeline_rf, Boston, "dis", the_explainer_rf, the_pred_function)
+
+pdp_2_contrib(modelo_pipeline_lm, Boston, "indus", the_explainer_lm, the_pred_function)
+pdp_2_contrib(modelo_pipeline_rf, Boston, "indus", the_explainer_rf, the_pred_function)
 
 
 # Shap --------------------------------------------------------------------
+
+the_pred_function(modelo_pipeline_lm, iris) %>% mean()
+
+avg_iris <- iris %>% 
+  mutate_if(is.numeric, ~ mean(.)) %>% 
+  mutate_if(is.factor, ~ get_mode(.))
+
+the_pred_function(modelo_pipeline_lm, avg_iris)
+
+
 
 the_explainer_rf
 predict(the_explainer_rf, iris[1,])
@@ -293,12 +301,25 @@ shap_1_rf
 
 shap_1_rf %>% as_tibble() %>% 
   group_by(variable_name) %>% 
-  summarise(avg = mean(contribution)) %>% 
-  spread(variable_name, avg)
+  summarise(shp = mean(contribution)) %>% 
+  spread(variable_name, shp)
+
+### 
+shap_1_lm <- predict_parts(explainer = the_explainer_lm, 
+                           new_observation = iris[1,], 
+                           type = "shap",
+                           B = 25)
+plot(shap_1_lm)
+
+shap_1_lm
+
+shap_1_lm %>% as_tibble() %>% 
+  group_by(variable_name) %>% 
+  summarise(shp = mean(contribution)) %>% 
+  spread(variable_name, shp)
 
 
-
-# Codigo antiguo ----------------------------------------------------------
+###
 
 
 
@@ -372,8 +393,122 @@ shap_contribs <- function(in_model,
   
 }
 
+### 
 
-shap_contribs(modelo_pipeline_lm, 
-              iris, 
-              c("Sepal.Width", "Petal.Length", "Petal.Width", "Species"),
-              the_pred_function)
+library(ranger)
+
+data("Boston", package = "MASS")
+
+x_var <- c("lstat", "rm", "dis", "indus")
+y_var <- "medv"
+
+Boston_train <- Boston[-1:-6, c(x_var, y_var)]
+Boston_test  <- Boston[1:6, x_var]
+
+f <- as.formula(paste(y_var, "~", paste(x_var, collapse = "+")))
+model_lm <- lm(f, data = Boston_train)
+model_rf <- ranger(f, data = Boston_train)
+
+## LM
+contribs_lm <- model.matrix(f, Boston_train) %>%
+  sweep(2, coef(model_lm), "*") %>%
+  as_tibble() %>%
+  mutate(pred = rowSums(.))
+head(contribs_lm)
+
+# Compute approximate Shapley values using 10 Monte Carlo simulations
+set.seed(101)  # for reproducibility
+shap_lm <- fastshap::explain(model_lm,
+                             X = subset(Boston_train, select = -medv),
+                             nsim = 10,
+                             pred_wrapper = predict,
+                             adjust = TRUE)
+shap_lm
+
+shap_contribs_lm <- get_shap_contribs(Boston_train, shap_lm, model_lm, predict, pred = TRUE)
+shap_contribs_lm
+head(contribs_lm)
+
+plot(Boston_train$lstat, shap_contribs_lm$lstat)
+
+## RF
+
+# Compute approximate Shapley values using 10 Monte Carlo simulations
+set.seed(101)  # for reproducibility
+shap_rf <- fastshap::explain(model_rf,
+                             X = subset(Boston_train, select = -medv),
+                             nsim = 10,
+                             pred_wrapper = function(object, newdata) predict(object, newdata)$predictions,
+                             adjust = TRUE)
+shap_rf
+
+shap_contribs_rf <- get_shap_contribs(Boston_train, shap_rf,
+                                      model_rf,
+                                      function(object, newdata) predict(object, newdata)$predictions,
+                                      pred = TRUE)
+shap_contribs_rf
+
+plot(Boston_train$lstat, shap_contribs_rf$lstat)
+plot(Boston_train$rm, shap_contribs_rf$rm)
+plot(Boston_train$dis, shap_contribs_rf$dis)
+plot(Boston_train$indus, shap_contribs_rf$indus)
+
+
+shap_contribs(in_model = model_rf,
+              in_data = Boston_train,
+              pred_vars = NULL,
+              pred_fun = function(object, newdata) predict(object, newdata)$predictions,
+              nsim = 10,
+              grid_resolution = 20,
+              seed = NULL)
+
+
+###
+library(rcontribs)
+
+pdp_pred <- function(object, newdata)  {
+  results <- as.vector(predict(object, newdata))
+  return(results)
+}
+
+res <- pdp_1_contrib(model_lm, Boston, "lstat", pdp_pred)
+
+autoplot(res) +
+  geom_abline(intercept = 0, slope = as.numeric(coef(m_lm)["lstat"]), 
+              color = "red", linetype = 2) +
+  coord_cartesian(ylim = c(-20, 30))
+
+res <- pdp_1_contrib(m_lm, Boston, "black", pdp_pred_lm)
+
+autoplot(res) +
+  geom_abline(intercept = 0, slope = as.numeric(coef(m_lm)["black"]), color = "red") +
+  coord_cartesian(ylim = c(-20, 30))
+
+res_all_lm <- pdp_contribs(model_lm, Boston, 
+                           Boston %>% select(-medv) %>% names(), 
+                           pdp_pred)
+
+probe_lm <- res_all_lm$contribs %>% mutate(y_hat = rowSums(.))
+probe_lm
+
+plot(predictions_lm, probe_lm$y_hat)
+abline(a= 0, b = 1)
+
+probe_lm %>% summarise_all(~ sum(.))
+sum(predictions_lm)
+
+contribs_lm %>% summarise_all(~ sum(.))
+
+curve(res_all_lm$contrib_funs$crim(x), from = min(Boston$crim), to = max(Boston$crim))
+curve(res_all_lm$contrib_funs$zn(x), from = min(Boston$zn), to = max(Boston$zn))
+curve(res_all_lm$contrib_funs$indus(x), from = min(Boston$indus), to = max(Boston$indus))
+curve(res_all_lm$contrib_funs$chas(x), from = min(Boston$chas), to = max(Boston$chas))
+curve(res_all_lm$contrib_funs$nox(x), from = min(Boston$nox), to = max(Boston$nox))
+curve(res_all_lm$contrib_funs$rm(x), from = min(Boston$rm), to = max(Boston$rm))
+curve(res_all_lm$contrib_funs$age(x), from = min(Boston$age), to = max(Boston$age))
+curve(res_all_lm$contrib_funs$dis(x), from = min(Boston$dis), to = max(Boston$dis))
+curve(res_all_lm$contrib_funs$rad(x), from = min(Boston$rad), to = max(Boston$rad))
+curve(res_all_lm$contrib_funs$tax(x), from = min(Boston$tax), to = max(Boston$tax))
+curve(res_all_lm$contrib_funs$ptratio(x), from = min(Boston$ptratio), to = max(Boston$ptratio))
+curve(res_all_lm$contrib_funs$black(x), from = min(Boston$black), to = max(Boston$black))
+curve(res_all_lm$contrib_funs$lstat(x), from = min(Boston$lstat), to = max(Boston$lstat))
